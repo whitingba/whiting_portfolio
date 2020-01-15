a03530f52be97d2c9de6db8ece08513ef6f83e05
Barbara Whiting <whitingba@gmail.com>
2020-01-15 11:36:01 -0700

    working contact form and links to resume, linkedIn, and github added to contact me section

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import Navbar from './components/Navbar/index';
 import Parallax1 from './components/Parallax1';
-import AboutMe from './components/AboutMe'
+import AboutMe from './components/AboutMe';
 import Parallax2 from './components/Parallax2';
 import Projects from './components/Projects/index'
 import Parallax3 from './components/Parallax3';
+import Contact from './components/Contact';
 
 
 
@@ -18,7 +19,7 @@ function App() {
       <Parallax2 />
       <Projects />
       <Parallax3 />
-      {/* contact component */}
+      <Contact />
 
     </>
   );
---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './style.css';
+
+
+const index = () => {
+    return (
+        <div>
+
+            <h3 className="header">Contact Me</h3>
+
+            <a href="https://docs.google.com/document/d/15PZd1-Z35F6hq3ztIfiM7DcTwGQ1VHOJurc9Pv538Xo/edit?usp=sharing" target="_blank" rel="noopener noreferrer" ><i className="fa fa-file"></i></a>
+            <a href="https://www.linkedin.com/in/barbara-whiting" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin"></i></a>
+            <a href="https://github.com/whitingba?tab=repositories" target="_blank" rel="noopener noreferrer" ><i class="fa fa-github"></i></a>
+
+            <div class="inbound-form-wrapper" id="form_2318" data-path="https://app.99inbound.com/i/5c1bc766-3498-413b-b2b5-a6853b16e202" data-token="TdZs6pOVw2soVZsg6ot3egtt"></div>
+
+
+
+
+        </div>
+    );
+};
+
+export default index;
